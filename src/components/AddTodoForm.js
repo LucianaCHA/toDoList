c5c28afdf0cb9ea889de8b0d7a3b2d0f78ea3d7d
Luciana Chamorro <76781967+LucianaCHA@users.noreply.github.com>
2022-07-21 14:05:43 -0300

    Add files via upload

---
+++ src/components/AddTodoForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/todoSlice';
+
+
+const AddTodoForm = () => {
+	const dispatch = useDispatch();
+	const [value, setValue] = useState('');
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		dispatch(addTodo({
+			title: value,
+		}));
+		setValue('');
+		console.log('task dispatched',value);
+	};
+
+
+	return (
+		<form onSubmit={onSubmit} className='form-inline mt-3 mb-3'>
+			<label className='sr-only'>Name</label>
+			<input
+				type='text'
+				className='form-control mb-2 mr-sm-2'
+				placeholder='Add todo...'
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			></input>
+
+			<button type='submit' className='btn btn-primary mb-2'>
+				Submit
+			</button>
+		</form>
+	);
+};
+
+export default AddTodoForm;
